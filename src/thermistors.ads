d5baec817fe80d94226076b9b3776f33ab6485f0
Liam Powell <liam@liampwll.com>
2024-07-11 00:24:33 +0800

    Correct thermistor loop frequency.

--- src/thermistors.ads
+++ src/thermistors.ads
@@ -4,7 +4,7 @@ with Physical_Types;         use Physical_Types;
 
 package Thermistors is
 
-   Loop_Frequency : constant Frequency := (150_000.0 / 4.0) * hertz / (256.0 * 4.0 * 640.5);
+   Loop_Frequency : constant Frequency := (150_000_000.0 / 4.0) * hertz / (256.0 * 4.0 * 640.5);
    --  150/4 = ADC clock frequency.
    --  256 = Oversampling.
    --  4 = Thermistor count.
